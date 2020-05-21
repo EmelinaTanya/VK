f31ba630ebcb8f4872879de10f620699a77f667c
EmelinaTanya <sindi76@mail.ru>
2020-05-21 15:24:52 +0400

    added PostManager & Attachment

---
+++ src/main/java/ru/netology/domain/attachment/PhotoAttachment.java
@@ -0,0 +1,13 @@
+package ru.netology.domain.attachment;
+
+public class PhotoAttachment {
+    private PhotoAttachment[] photoAttachment;
+
+        public PhotoAttachment[] attachment (int id, int albumId, int ownerId, int userId, String text, int date, String[] sizesInfo, int width, int height) {
+            return null;
+        }
+}
+
+
+
+
---
+++ src/main/java/ru/netology/domain/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.domain.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] search(int ownerId, int postId, String namePost, int data, String textPost) {
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+        return;
+    }
+
+}
